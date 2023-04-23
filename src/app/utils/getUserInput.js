52f5a5b52cef877593d8b440e501e4bf4a159f64
dumbus <dumbusx@gmail.com>
2023-04-23 04:41:03 +0300

    feat: move to node version 18.14.0

--- src/app/utils/getUserInput.js
+++ src/app/utils/getUserInput.js
@@ -1,27 +1,13 @@
 import { stdin as input, stdout as output } from 'node:process';
-
-// node 18.x (18.14.0)
-// import readline from 'node:readline/promises';
-
-// node 15.x (15.8.0)
-import readline from 'node:readline';
+import readline from 'node:readline/promises';
 
 import { messages } from './messenger.js';
 
 const getData = async () => {
   const rl = readline.createInterface({ input, output });
 
-  // node 18.x (18.14.0)
-  // const polynomial = await rl.question(messages.inputPolynomialMsg);
-  // const listNumber = await rl.question(messages.inputListNumberMsg);
-
-  // node 15.x (15.8.0)
-  const polynomial = await new Promise(resolve => {
-    rl.question(messages.inputPolynomialMsg, resolve);
-  });
-  const listNumber = await new Promise(resolve => {
-    rl.question(messages.inputListNumberMsg, resolve);
-  });
+  const polynomial = await rl.question(messages.inputPolynomialMsg);
+  const listNumber = await rl.question(messages.inputListNumberMsg);
 
   const data = { polynomial, listNumber };
 
@@ -34,9 +20,7 @@ const getExitCommand = async () => {
   const rl = readline.createInterface({ input, output });
 
   console.log(messages.emptyLineMsg);
-  await new Promise(resolve => {
-    rl.question(messages.inputExitCommand, resolve);
-  });
+  await rl.question(messages.inputExitCommand);
 
   rl.close();
 };
